ba4d8d1d18f14cbdd5f450cedf222fe277303e7b
Egor <egork2205@gmail.com>
2025-04-12 17:53:55 +0700

    Добавьте файлы проекта.

---
+++ TasksFromModul3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TasksFromModul3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName = "Egor";
+            Console.WriteLine(MyName);
+        }
+    }
+}
